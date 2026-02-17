23c1de939b095ad7b7717a78b46e000d87592980
Joe simpson <joe@egmrc.com>
2026-02-17 14:23:49 -0700

    Extract reusable `fetchFromAdOrbit` function to simplify API calls
    - Abstracted repeated AdOrbit API call logic into a reusable `fetchFromAdOrbit` function for better code organization.
    - Updated `GET` handler to use the new function, reducing duplication and improving readability.

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,7 +2,39 @@ import {NextRequest} from "next/server";
 import axios from 'axios';
 import CryptoJS from 'crypto-js';
 
-export async function GET(req: NextRequest){
+
+async function fetchFromAdOrbit(url: string, method: string, headers?: {}) {
+    const API_KEY = process.env.API_KEY;
+    const API_PUB = process.env.PUBLIC_KEY;
+
+
+
+    // Check if API key is provided
+    if (!API_KEY) {
+        console.error('Error: API_KEY is required. Please set it in your .env file.');
+        process.exit(1);
+    }
+
+
+    const msg = method.toUpperCase() + "\n" + url;
+
+    const hash = CryptoJS.HmacSHA512(msg, API_KEY);
+    const crypt = CryptoJS.enc.Utf8.parse(hash.toString());
+    const base64 = CryptoJS.enc.Base64.stringify(crypt);
+
+    const res = await axios.get(url, {
+        headers: {
+            Authorization: "ADORBIT " + API_PUB + ":" + base64,
+            Accept: 'application/json',
+            Method: method,
+            ...headers
+        }
+    });
+
+    return res.data;
+}
+
+export async function GET(req: NextRequest) {
 
 // Configuration
     const API_BASE_URL = process.env.API_BASE_URL || 'https://api.adorbit.com';
@@ -14,30 +46,10 @@ export async function GET(req: NextRequest){
         console.error('Error: API_KEY is required. Please set it in your .env file.');
         process.exit(1);
     }
-    const routeMsg = "GET\n"+API_BASE_URL+"/";
 
+    const routes = await fetchFromAdOrbit(API_BASE_URL+"/", 'GET');
 
-    var routeHash   = CryptoJS.HmacSHA512(routeMsg, API_KEY);
-    var routeCrypt  = CryptoJS.enc.Utf8.parse(routeHash.toString());
-    var routeBase64 = CryptoJS.enc.Base64.stringify(routeCrypt);
-
-    const routes = await axios.get(API_BASE_URL,{
-        headers: {
-            'Authorization': "ADORBIT " +API_PUB+":"+routeBase64,
-            'Accept': 'application/json',
-        }
-    });
-
-    const ticketRoute:string = routes.data.tickets;
-
-
-
-    const ticketMsg = "GET\n"+ticketRoute;
-
-
-    const ticketHash = CryptoJS.HmacSHA512(ticketMsg, API_KEY);
-    const crypt = CryptoJS.enc.Utf8.parse(ticketHash.toString());
-    const base64 = CryptoJS.enc.Base64.stringify(crypt);
+    const ticketRoute: string = routes.tickets;
 
     function getSixMonthsAgo() {
         const today = new Date();
@@ -57,15 +69,21 @@ export async function GET(req: NextRequest){
         return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
     }
 
-    const ticketRes = await axios.get(ticketRoute, {
-        headers: {
-            'Authorization': "ADORBIT " +API_PUB+":"+base64,
-            'Accept': 'application/json',
+    const tickets = await fetchFromAdOrbit(ticketRoute, "GET",
+        {
             'X-OPT-CHANGEDSINCE': getSixMonthsAgo()
         }
-    })
+    )
 
-    const tickets = ticketRes.data;
+/*
+    if we have to fetch the issue id I think we'd have to do all this:
+    const oneticketRoute: string = routes["ad-ticket"];
+    const adRoute: string = routes["print-ad-item"];
+
+
+    const ticket = await fetchFromAdOrbit(oneticketRoute.replace("{id}", "16267"), "GET");
+
+    const ad = await fetchFromAdOrbit(adRoute.replace("{id}", ticket.ad_id), "GET");*/
 
     const activeTickets = tickets.filter((ticket: { [x: string]: string; }) => ticket['status_name'] !== 'Done');
 
